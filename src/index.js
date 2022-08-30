5e9219a3f2b05d3759d0f20546da461c724f6d20
Alena home <79262812867@ya.ru>
2022-08-30 22:08:11 +0300

    урок 1.8 5 мин

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/app/';
+/* import App from './components/app/'; */
+
+function WhoAmI (props){
+  return (
+    <>
+      <h1>My name is {props.name}, surname - {props.surname}</h1>
+      <a href={props.link}>My profile</a>
+    </>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WhoAmI name="Jon" surname="Smit" link="yandex.ru"/>
   </React.StrictMode>
 );
 
