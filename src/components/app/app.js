0735323771630203e4cb1e6fa2f1a6b7e9d6e347
Alena home <79262812867@ya.ru>
2022-09-04 22:36:38 +0300

    1 11

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 
 import AppHeader from "../app-header";
@@ -8,28 +8,78 @@ import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 
 import './app.css';
+import styled from "styled-components";
 
-const App = () => {
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
 
-    const data = [
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            data: [
+                {label: 'Going to learn React', important: true, id: 1},
+                {label: 'That is so good', important: false, id: 2},
+                {label: 'I nead a break...', important: false, id: 3}
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+
+        this.maxId =4;
+
+    }
+    
+    deleteItem(id) {
+        this.setState(({data})=> {
+            const index = data.findIndex(elem => elem.id === id);
+            
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArr = [...before, ...after];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        })
         
-        {label: 'Going to learn React', important: true, id: 'dsds'},
-        {label: 'That is so good', important: false, id: 'ffd'},
-        {label: 'I nead a break...', important: false, id: 'fgf'}
-    ];
+    }
 
+   render () {
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList posts={data} />
-            <PostAddForm/>
-        </div>
+            <PostList 
+                posts={this.state.data}
+                onDelete={this.deleteItem} />
+            <PostAddForm
+                onAdd={this.addItem} />
+        </AppBlock>
         
         )
+   }
 }
 
-export default App;
