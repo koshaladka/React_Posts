f1193319c8bf2f399a4c476b11e81b19a10893e4
Alena home <79262812867@ya.ru>
2022-09-05 15:54:28 +0300

    1 13 первая часть закончена

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,7 +5,7 @@ import PostListItem from '../post-list-item';
 import { ListGroup } from 'reactstrap';
 import './post-list.css'
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map( (item) => {
         // Простой способ проверки на объект + содержится ли в нем информация
@@ -15,7 +15,9 @@ const PostList = ({posts, onDelete}) => {
                 <li key = {id} className='list-group-item'>
                     <PostListItem
                          {...itemProps}
-                         onDelete={() => onDelete(id)} />
+                         onDelete={() => onDelete(id)}
+                         onToggleImportant={() => onToggleImportant(id)}
+                         onToggleLiked = {() => onToggleLiked(id)} />
                 </li>
             )
         }
